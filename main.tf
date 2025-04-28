08ccd754ce34a2abbb0c8dd44371db4e821bfd52
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-28 09:00:35 -0400

    adding ingress rule

--- main.tf
+++ main.tf
@@ -11,3 +11,25 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+variable "ingress_rules" {
+  description = "A map of ingress rules"
+  type = map(object({
+    cidr_ipv4   = list(string)
+    from_port   = number
+    ip_protocol = string
+    to_port     = number
+    desciption  = string
+  }))
+}
+resource "aws_security_group_rule" "ingress" {
+  for_each = var.ingress_rules
+
+  type              = "ingress"
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.ip_protocol
+  cidr_blocks       = each.value.cidr_ipv4
+  security_group_id = aws_security_group.allow_tls.id
+
+  description = each.value.desciption
+}
